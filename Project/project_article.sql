47a5b12a7a997e5fe6b5cf4328586947c2906133
ElwinSuLiu <50895085+ElwinSuLiu@users.noreply.github.com>
2019-09-18 15:39:15 +1200

    Add files via upload

---
+++ Project/project_article.sql
@@ -0,0 +1,64 @@
+-- MySQL dump 10.13  Distrib 8.0.13, for Win64 (x86_64)
+--
+-- Host: mysql.learn.cpp.ac.nz    Database: project
+-- ------------------------------------------------------
+-- Server version	8.0.11
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `article`
+--
+
+DROP TABLE IF EXISTS `article`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `article` (
+  `articleID` int(11) NOT NULL,
+  `titleID` int(11) DEFAULT NULL,
+  `categoryID` int(11) DEFAULT NULL,
+  `typeID` int(11) DEFAULT NULL,
+  `name` varchar(30) DEFAULT NULL,
+  `born` year(4) DEFAULT NULL,
+  `died` year(4) DEFAULT NULL,
+  `nationality` varchar(20) DEFAULT NULL,
+  `knownFor` varchar(30) DEFAULT NULL,
+  `notableWorks` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`articleID`),
+  KEY `titleID` (`titleID`),
+  KEY `categoryID` (`categoryID`),
+  KEY `typeID` (`typeID`),
+  CONSTRAINT `article_ibfk_1` FOREIGN KEY (`titleID`) REFERENCES `articledetails` (`titleid`),
+  CONSTRAINT `article_ibfk_2` FOREIGN KEY (`categoryID`) REFERENCES `category` (`categoryid`),
+  CONSTRAINT `article_ibfk_3` FOREIGN KEY (`typeID`) REFERENCES `type` (`typeid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `article`
+--
+
+LOCK TABLES `article` WRITE;
+/*!40000 ALTER TABLE `article` DISABLE KEYS */;
+/*!40000 ALTER TABLE `article` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-09-18 15:37:53
